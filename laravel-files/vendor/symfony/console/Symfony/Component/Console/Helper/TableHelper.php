c3aa58e2955d7f3a8eaca04143f55c7d906dbb2e
asaenz <asaenz@businessolver.com>
2014-05-08 13:53:57 -0500

    update laravel

--- laravel-files/vendor/symfony/console/Symfony/Component/Console/Helper/TableHelper.php
+++ laravel-files/vendor/symfony/console/Symfony/Component/Console/Helper/TableHelper.php
@@ -161,6 +161,29 @@ class TableHelper extends Helper
     {
         $this->rows[] = array_values($row);
 
+        $keys = array_keys($this->rows);
+        $rowKey = array_pop($keys);
+
+        foreach ($row as $key => $cellValue) {
+            if (!strstr($cellValue, "\n")) {
+                continue;
+            }
+
+            $lines = explode("\n", $cellValue);
+            $this->rows[$rowKey][$key] = $lines[0];
+            unset($lines[0]);
+
+            foreach ($lines as $lineKey => $line) {
+                $nextRowKey = $rowKey + $lineKey + 1;
+
+                if (isset($this->rows[$nextRowKey])) {
+                    $this->rows[$nextRowKey][$key] = $line;
+                } else {
+                    $this->rows[$nextRowKey] = array($key => $line);
+                }
+            }
+        }
+
         return $this;
     }
 
@@ -290,7 +313,7 @@ class TableHelper extends Helper
     /**
      * Sets cell padding type.
      *
-     * @param integer $padType STR_PAD_*
+     * @param int     $padType STR_PAD_*
      *
      * @return TableHelper
      */
@@ -391,7 +414,7 @@ class TableHelper extends Helper
      * Renders table cell with padding.
      *
      * @param array   $row
-     * @param integer $column
+     * @param int     $column
      * @param string  $cellFormat
      */
     private function renderCell(array $row, $column, $cellFormat)
@@ -403,6 +426,8 @@ class TableHelper extends Helper
         if (function_exists('mb_strlen') && false !== $encoding = mb_detect_encoding($cell)) {
             $width += strlen($cell) - mb_strlen($cell, $encoding);
         }
+
+        $width += $this->strlen($cell) - $this->computeLengthWithoutDecoration($cell);
 
         $content = sprintf($this->cellRowContentFormat, $cell);
 
@@ -432,7 +457,7 @@ class TableHelper extends Helper
     /**
      * Gets column width.
      *
-     * @param integer $column
+     * @param int     $column
      *
      * @return int
      */
@@ -455,21 +480,13 @@ class TableHelper extends Helper
      * Gets cell width.
      *
      * @param array   $row
-     * @param integer $column
+     * @param int     $column
      *
      * @return int
      */
     private function getCellWidth(array $row, $column)
     {
-        if ($column < 0) {
-            return 0;
-        }
-
-        if (isset($row[$column])) {
-            return $this->strlen($row[$column]);
-        }
-
-        return $this->getCellWidth($row, $column - 1);
+        return isset($row[$column]) ? $this->computeLengthWithoutDecoration($row[$column]) : 0;
     }
 
     /**
@@ -481,8 +498,20 @@ class TableHelper extends Helper
         $this->numberOfColumns = null;
     }
 
+    private function computeLengthWithoutDecoration($string)
+    {
+        $formatter = $this->output->getFormatter();
+        $isDecorated = $formatter->isDecorated();
+        $formatter->setDecorated(false);
+
+        $string = $formatter->format($string);
+        $formatter->setDecorated($isDecorated);
+
+        return $this->strlen($string);
+    }
+
     /**
-     * {@inheritDoc}
+     * {@inheritdoc}
      */
     public function getName()
     {
